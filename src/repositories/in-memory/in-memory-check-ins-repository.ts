4ee8f90f6548d475b6401ebf06a79ab9266d78ce
GuiLeoni14 <guileone167@gmail.com>
2023-07-08 14:30:02 -0300

    feat: fetch-nearby-gyms

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -4,8 +4,20 @@ import { randomUUID } from 'node:crypto'
 import dayjs from 'dayjs'
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
+  private items: CheckIn[] = []
+
   async countByUserId(userId: string) {
     return this.items.filter((item) => item.user_id === userId).length
+  }
+
+  async findById(id: string) {
+    const checkIn = this.items.find((item) => item.id === id)
+
+    if (!checkIn) {
+      return null
+    }
+
+    return checkIn
   }
 
   async findManyByUserId(userId: string, page: number) {
@@ -29,8 +41,6 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return checkInOnSameDate ?? null
   }
 
-  public items: CheckIn[] = []
-
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
       id: randomUUID(),
@@ -41,6 +51,16 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     }
 
     this.items.push(checkIn)
+
+    return checkIn
+  }
+
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id)
+
+    if (checkInIndex >= 0) {
+      this.items[checkInIndex] = checkIn
+    }
 
     return checkIn
   }
